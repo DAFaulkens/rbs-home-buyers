8143afd378f4235d82f0f148f7a959412ed8446a
Fred Siika <fredsiika@gmail.com>
2020-03-10 23:03:47 -0500

    Added phoneValidation script

--- assets/js/theme-custom.js
+++ assets/js/theme-custom.js
@@ -9,70 +9,75 @@ const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+	e.preventDefault();
 
-    checkInputs();
-    
-    console.log(`Last name: ${lastName.value}`);
-    console.log(`Email: ${email.value}`);
-    console.log(`Phone: ${phone.value}`);
+	checkInputs();
 });
 
 function checkInputs() {
-    // Trim white space
-    const firstNameValue = firstName.value.trim();
-    const lastNameValue = lastName.value.trim();
-    const emailValue = email.value.trim();
-    const phoneValue = phone.value.trim();
+	// Trim white space
+	const firstNameValue = firstName.value.trim();
+	const lastNameValue = lastName.value.trim();
+	const emailValue = email.value.trim();
+	const phoneValue = phone.value.trim();
 
-    // Check first name
-    if(firstNameValue === '') {
-        setErrorFor(firstName, 'Oops, first name cannot be blank.')
-        console.log(`First name: Oops, first name cannot be blank.`);
-    } else {
-        setSuccessFor(firstName);
-        console.log(`First name: ${firstName.value}`); 
-    }
+	// Check first name
+	if(firstNameValue === '') {
+		setErrorFor(firstName, 'First name cannot be blank.')
+	} else {
+		setSuccessFor(firstName);
+	}
 
-    // Check last name
-    if(lastNameValue === '') {
-        setErrorFor(lastName, 'Oops, last name cannot be blank.');
-        console.log('Oops, last name cannot be blank.');
-    } else {
-        setSuccessFor(lastName);
-    }
+	// Check last name
+	if(lastNameValue === '') {
+		setErrorFor(lastName, 'Last name cannot be blank.');
+	} else {
+		setSuccessFor(lastName);
+	}
 
-    // Check email address
-    if(emailValue === '') {
-        setErrorFor(email, 'Oops, email cannot be blank.');
-    } else if(!isEmail(emailValue)) {
-        setErrorFor(email, 'Oops, not a valid email.');
-        console.log('Oops, not a valid email.');
-    } else {
-        setSuccessFor(email);
-    }
+	// Check email address
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank.');
+	} else if(!isValidEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email.');
+	} else {
+		setSuccessFor(email);
+	}
 
-    // Check phone number 
+	// Check phone number
+	if(phoneValue === '') {
+		setErrorFor(phone, 'Phone number cannot be blank');
+	} else if(!isValidPhone(phoneValue)) {
+		setErrorFor(phone, 'Not a valid phone number.');
+	} else {
+		setSuccessFor(phone);
+		formatPhone(phone);
+	}
 };
 
 function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
-    formControl.className = 'form-control error';
-    small.innerText = message;
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
 };
 
 function setSuccessFor(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success ';
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
 };
 
-function isEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-    console.log(`Email valid: ${isEmail(email)}`);
+function isValidEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 };
 
-function isPhone(phone) {
-
+function isValidPhone(phone) {
+	return /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(phone);
 };
 
+function formatPhone(phone) {
+	const formControl = input.parentElement;
+	const phoneNum = formControl.getElementById('phone');
+	formControl.className = 'form-control success';
+	phoneNum.input = phone;
+};
